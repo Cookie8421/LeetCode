e534bd766cf70cd580b8bad646929ef055d95eb9
joey44 <cookie_8421@sina.cn>
2019-03-27 16:55:56 +0800

    ****

--- src/main/java/com/leetcode/algorithms/MediumMode/StringtoInteger_atoi.java
+++ src/main/java/com/leetcode/algorithms/MediumMode/StringtoInteger_atoi.java
@@ -76,29 +76,5 @@ public class StringtoInteger_atoi {
 
         System.out.println(myAtoi("2147483648"));
 
-        //访问接口的代码
-        Map map = new HashMap();
-
-        //map.put("dateLine", "2019-3-22");
-        //map.put("phoneNum", "17372216333");
-        //map.put("amount", "2000");
-        //map.put("serialNum", "8190319161352113");
-        //map.put("merchantNo", "17033681");
-        Gson gson = new Gson();
-        String result = gson.toJson(map);
-        System.out.println(result);
-        byte[] aesResult = AESUtil.encrypt(result, "1");
-        String encryptResultStr = AESUtil.parseByte2HexStr(aesResult);
-        String sign = Encodes.md5Encode(Encodes.md5Encode(result));
-
-
-        System.out.println("sign:"+sign);
-        System.out.println("data:"+encryptResultStr);
-
-
-        //接口这样接收
-        byte[] decryptFrom = AESUtil.parseHexStr2Byte(encryptResultStr);
-        String r = new String(AESUtil.decrypt(decryptFrom, "1"));
-        System.out.println("真实数据:"+new String(AESUtil.decrypt(decryptFrom, "1")));
     }
 }
