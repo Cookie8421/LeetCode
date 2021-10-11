0a3de799d6355601df105c8ade7c45352b0ef580
杨瀚文 <Cookie_8421@sina.cn>
2021-10-11 18:33:51 +0800

    IOCTest

---
+++ src/main/java/com/leetcode/algorithms/Custom/JavaIOC/TestBean.java
@@ -0,0 +1,8 @@
+package com.leetcode.algorithms.Custom.JavaIOC;
+
+public class TestBean {
+
+    private int beanId;
+
+    private String beanName;
+}
---
+++ src/main/java/com/leetcode/algorithms/MediumMode/ContainsDuplicate3.java
@@ -0,0 +1,13 @@
+package com.leetcode.algorithms.MediumMode;
+
+/**
+ * @author YHW
+ * @ClassName: ContainerWithMostWater
+ * @Description:
+ * Given an integer array nums and two integers k and t, return true if there are two distinct indices i and j in the array such that abs(nums[i] - nums[j]) <= t and abs(i - j) <= k.
+ */
+public class ContainsDuplicate3 {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        return false;
+    }
+}
