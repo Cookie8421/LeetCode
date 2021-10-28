1d2444b025994ccdbfbb9593cb4640b042787137
杨瀚文 <Cookie_8421@sina.cn>
2021-10-28 18:07:37 +0800

    Medium:1 & more notation

--- src/main/java/com/leetcode/algorithms/MediumMode/MinimumSizeSubArraySum.java
+++ src/main/java/com/leetcode/algorithms/MediumMode/MinimumSizeSubArraySum.java
@@ -8,7 +8,7 @@ package com.leetcode.algorithms.MediumMode;
  * Given an array of positive integers nums and a positive integer target,
  * return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target.
  * If there is no such subarray, return 0 instead.
- * @date 2021/10/11 11:04
+ * @date 2021/10/28 11:04
  */
 public class MinimumSizeSubArraySum {
 
