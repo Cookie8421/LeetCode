1ddf488f262be3215aeffb69e07e25389098925c
杨瀚文 <Cookie_8421@sina.cn>
2021-11-17 10:45:31 +0800

    Medium

--- src/main/java/com/leetcode/algorithms/MediumMode/LowestCommonAncestorOfABinaryTree.java
+++ src/main/java/com/leetcode/algorithms/MediumMode/LowestCommonAncestorOfABinaryTree.java
@@ -10,6 +10,15 @@ package com.leetcode.algorithms.MediumMode;
  * According to the definition of LCA on Wikipedia:
  * “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants
  * (where we allow a node to be a descendant of itself).”
+ *
+ * Constraints:
+ *
+ *     The number of nodes in the tree is in the range [2, 105].
+ *     -109 <= Node.val <= 109
+ *     All Node.val are unique.
+ *     p != q
+ *     p and q will exist in the tree.
+ *
  */
 public class LowestCommonAncestorOfABinaryTree {
 
