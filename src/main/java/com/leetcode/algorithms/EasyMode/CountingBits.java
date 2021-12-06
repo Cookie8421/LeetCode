71ddffd60e7698ac1eda8bf213ab8a077349cc6d
杨瀚文 <Cookie_8421@sina.cn>
2021-12-06 11:08:01 +0800

    Medium

---
+++ src/main/java/com/leetcode/algorithms/EasyMode/CountingBits.java
@@ -0,0 +1,69 @@
+package com.leetcode.algorithms.EasyMode;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * @Author JoeyYoung
+ * @ClassName: Counting Bits
+ * @Date 2021/12/3 11:00
+ * @Description:
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+ * ans[i] is the number of 1's in the binary representation of i.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: [0,1,1]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ *
+ * Example 2:
+ *
+ * Input: n = 5
+ * Output: [0,1,1,2,1,2]
+ * Explanation:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 3 --> 11
+ * 4 --> 100
+ * 5 --> 101
+ *
+ *
+ *
+ * Constraints:
+ *
+ *     0 <= n <= 105
+ *
+ *
+ *
+ * Follow up:
+ *
+ *     It is very easy to come up with a solution with a runtime of O(n log n).
+ *     Can you do it in linear time O(n) and possibly in a single pass?
+ *     Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+ */
+public class CountingBits {
+
+    public int[] countBits(int n) {
+        int[] ans=new int[n+1];
+        for(int i=1;i<=n;i++)
+        {
+            if((i^1)==(i+1)) ans[i]=ans[i>>1];
+            else ans[i]=ans[i>>1]+1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.stream(new CountingBits().countBits(22))
+                .mapToObj(i -> String.valueOf(i))
+                .collect(Collectors.joining(",")));
+    }
+
+}
