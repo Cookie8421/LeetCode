da4be968dd113f71bec029cefc2da7ad6f6298bb
JoeyYoung <Cookie_8421@sina.cn>
2022-09-15 18:00:35 +0800

    test

--- src/main/java/com/leetcode/Learning/Note/Test.java
+++ src/main/java/com/leetcode/Learning/Note/Test.java
@@ -1,56 +1,7 @@
 package com.leetcode.Learning.Note;
 
-
-import com.leetcode.algorithms.EasyMode.AddBinary;
-import com.leetcode.algorithms.MediumMode.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Test {
     public static void main(String[] args) {
-        /*
-        Pattern pattern = Pattern.compile("(?<=\\[)(.+?)(?=\\])");
-        Matcher matcher = pattern.matcher("SELECT" +
-                " t.id " +
-                " FROM" +
-                " `wa_site` t" +
-                " LEFT JOIN wa_elderly we ON ( we.community_code = t.community_code ) " +
-                " WHERE" +
-                " 1 = 1 " +
-                " AND t.del_flag = '0' " +
-                " AND t.`status` = '1' " +
-                " AND we.del_flag = '0' " +
-                " AND we.`status` = '1' " +
-                " AND we.mobile = [ cpn ] " +
-                " LIMIT 1");
-        while (matcher.find()) {
-            System.out.println(matcher.group());
-        }
-
-
-        List<List<Integer>> pairs = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        temp.add(0);
-        temp.add(3);
-        pairs.add(temp);
-        temp = new ArrayList<>();
-        temp.add(1);
-        temp.add(2);
-        pairs.add(temp);
-        temp = new ArrayList<>();
-        temp.add(0);
-        temp.add(2);
-        pairs.add(temp);
-        SmallestStringWithSwaps s = new SmallestStringWithSwaps();
-        System.out.println(s.smallestStringWithSwaps("dcab", pairs));
-         */
-
-
-
-
 
     }
 }
