eda1f975c6f7cc202c693b1b663e34ae8cf19107
JoeyYoung <Cookie_8421@sina.cn>
2022-10-12 17:15:15 +0800

    Test

--- src/main/java/com/leetcode/Learning/Note/Test.java
+++ src/main/java/com/leetcode/Learning/Note/Test.java
@@ -1,7 +1,20 @@
 package com.leetcode.Learning.Note;
 
+import com.leetcode.algorithms.MediumMode.*;
+
+import java.math.BigDecimal;
+
 public class Test {
     public static void main(String[] args) {
+        /*LetterTilePossibilities l = new LetterTilePossibilities();
+        System.out.println(l.numTilePossibilities("AAB"));*/
+
+        /*ThreeSumClosest t = new ThreeSumClosest();
+        System.out.println(t.threeSumClosest(new int[]{4,0,5,-5,3,3,0,-4,-5} , -2));*/
+
+        /*PathWithMaximumProbability p = new PathWithMaximumProbability();
+        System.out.println(p.maxProbability(3, new int[][]{{0,1},{1,2},{0,2}}, new double[]{0.5,0.5,0.2}, 0, 2));*/
+
 
     }
 }
