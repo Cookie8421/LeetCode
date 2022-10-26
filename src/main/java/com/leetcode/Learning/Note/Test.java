5d964c50e36a96633271dc36bfbc9abed61d2c9f
JoeyYoung <Cookie_8421@sina.cn>
2022-10-26 17:29:18 +0800

    Test

--- src/main/java/com/leetcode/Learning/Note/Test.java
+++ src/main/java/com/leetcode/Learning/Note/Test.java
@@ -1,13 +1,16 @@
 package com.leetcode.Learning.Note;
 
+import com.leetcode.algorithms.HardcoreMode.BasicCalculator;
+import com.leetcode.algorithms.HardcoreMode.MinimumWindowSubstring;
+import com.leetcode.algorithms.HardcoreMode.ShortestPalindrome;
 import com.leetcode.algorithms.MediumMode.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        /*LetterTilePossibilities l = new LetterTilePossibilities();
-        System.out.println(l.numTilePossibilities("AAB"));*/
 
         /*ThreeSumClosest t = new ThreeSumClosest();
         System.out.println(t.threeSumClosest(new int[]{4,0,5,-5,3,3,0,-4,-5} , -2));*/
@@ -15,6 +18,11 @@ public class Test {
         /*PathWithMaximumProbability p = new PathWithMaximumProbability();
         System.out.println(p.maxProbability(3, new int[][]{{0,1},{1,2},{0,2}}, new double[]{0.5,0.5,0.2}, 0, 2));*/
 
+        /*ShortestPalindrome s = new ShortestPalindrome();
+        System.out.println(s.shortestPalindrome("aacecaaa"));*/
+
+        /*TargetSum t = new TargetSum();
+        System.out.println(t.findTargetSumWays(new int[]{3,3,3,3,3}, 3));*/
 
     }
 }
