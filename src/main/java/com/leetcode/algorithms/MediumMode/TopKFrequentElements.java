f0915018785317bb7a7b9f88e7db75fed1d00b7e
JoeyYoung <Cookie_8421@sina.cn>
2023-04-17 09:35:16 +0800

    347. Top K Frequent Elements

---
+++ src/main/java/com/leetcode/algorithms/MediumMode/TopKFrequentElements.java
@@ -0,0 +1,66 @@
+package com.leetcode.algorithms.MediumMode;
+
+import java.util.*;
+
+/**
+ * Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,1,2,2,3], k = 2
+ * Output: [1,2]
+ * Example 2:
+ *
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 105
+ * -104 <= nums[i] <= 104
+ * k is in the range [1, the number of unique elements in the array].
+ * It is guaranteed that the answer is unique.
+ *
+ *
+ * Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ */
+public class TopKFrequentElements {
+
+    /**
+     * Runtime
+     * 13 ms
+     * Memory
+     * 45 MB
+     */
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+
+        TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
+        for(int num : map.keySet()){
+            int freq = map.get(num);
+            if(!freqMap.containsKey(freq)){
+                freqMap.put(freq, new LinkedList<>());
+            }
+            freqMap.get(freq).add(num);
+        }
+
+        int[] res = new int[k];
+
+        for(int i = 0; i < k;){
+            Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
+            List<Integer> entryValue = entry.getValue();
+            for(Integer integer : entryValue){
+                res[i] = integer;
+                i++;
+            }
+        }
+        return res;
+    }
+
+}
