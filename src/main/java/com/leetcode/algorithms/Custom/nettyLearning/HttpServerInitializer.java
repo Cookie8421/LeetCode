8756957f8c6639f720ac689d1cdea6a0ed2dc07a
JoeyYoung <Cookie_8421@sina.cn>
2023-08-30 14:40:11 +0800

    Time Protocal Server and Client

--- src/main/java/com/leetcode/algorithms/Custom/nettyLearning/HttpServerInitializer.java
+++ src/main/java/com/leetcode/algorithms/Custom/nettyLearning/HttpServerInitializer.java
@@ -17,9 +17,8 @@ public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel channel) throws Exception {
         ChannelPipeline pipeline = channel.pipeline();
-        //pipeline.addLast(new HttpServerCodec());// http 编解码
-        //pipeline.addLast("httpAggregator",new HttpObjectAggregator(512*1024)); // http 消息聚合器                                                                     512*1024为接收的最大contentlength
-        //pipeline.addLast(new HttpRequestHandler());// 请求处理器
-        pipeline.addLast(new TimeServerHandler());//实现Time协议的Handler
+        pipeline.addLast(new HttpServerCodec());// http 编解码
+        pipeline.addLast("httpAggregator",new HttpObjectAggregator(512*1024)); // http 消息聚合器                                                                     512*1024为接收的最大contentlength
+        pipeline.addLast(new HttpRequestHandler());// 请求处理器
     }
 }
---
+++ src/main/java/com/leetcode/algorithms/Custom/nettyLearning/TimeServer.java
@@ -0,0 +1,59 @@
+package com.leetcode.algorithms.Custom.nettyLearning;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import java.net.InetSocketAddress;
+
+public class TimeServer {
+
+    int port ;
+
+    public TimeServer(int port){
+        this.port = port;
+    }
+
+    public void start() throws Exception{
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        //EventLoopGroup 的实现用来处理不同的传输。在这个例子中我们实现了一个服务端的应用，
+        // 因此会有 2 个 NioEventLoopGroup 会被使用。第一个经常被叫做‘boss’，用来接收进来的连接。
+        // 第二个经常被叫做‘worker’，用来处理已经被接收的连接，一旦‘boss’接收到连接，就会把连接信息注册到‘worker’上。
+        // 如何知道多少个线程已经被使用，如何映射到已经创建的 Channel上都需要依赖于 EventLoopGroup 的实现，
+        // 并且可以通过构造函数来配置他们的关系。
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup work = new NioEventLoopGroup();
+        try {
+            bootstrap.group(boss,work)
+                    .handler(new LoggingHandler(LogLevel.DEBUG))
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = bootstrap.bind(new InetSocketAddress(port)).sync();
+            System.out.println(" server start up on port : " + port);
+            f.channel().closeFuture().sync();
+        } finally {
+            boss.shutdownGracefully();
+            work.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        TimeServer server = new TimeServer(8090);
+        server.start();
+    }
+
+}
