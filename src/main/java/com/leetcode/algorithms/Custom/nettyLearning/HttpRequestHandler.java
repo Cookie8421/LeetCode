4e615e282675f2270c4ad162776061260538ec99
JoeyYoung <Cookie_8421@sina.cn>
2023-08-30 10:01:30 +0800

    2366. Minimum Replacements to Sort the Array

--- src/main/java/com/leetcode/algorithms/Custom/nettyLearning/HttpRequestHandler.java
+++ src/main/java/com/leetcode/algorithms/Custom/nettyLearning/HttpRequestHandler.java
@@ -48,4 +48,23 @@ public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequ
         // 将html write到客户端
         ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
     }
+
+    /***
+     * 这个方法会在发生异常时触发
+     *
+     * @param ctx
+     * @param cause
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        /**
+         * exceptionCaught() 事件处理方法是当出现 Throwable 对象才会被调用，即当 Netty 由于 IO
+         * 错误或者处理器在处理事件时抛出的异常时。在大部分情况下，捕获的异常应该被记录下来 并且把关联的 channel
+         * 给关闭掉。然而这个方法的处理方式会在遇到不同异常的情况下有不 同的实现，比如你可能想在关闭连接之前发送一个错误码的响应消息。
+         */
+        // 出现异常就关闭
+        cause.printStackTrace();
+        ctx.close();
+    }
+
 }
