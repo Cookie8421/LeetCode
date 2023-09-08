b37f6fc8a2d969b0879c241c0974a877ac50b65f
JoeyYoung <Cookie_8421@sina.cn>
2023-09-08 10:00:46 +0800

    201. Bitwise AND of Numbers Range

---
+++ src/main/java/com/leetcode/algorithms/MediumMode/BitwiseANDOfNumbersRange.java
@@ -0,0 +1,63 @@
+package com.leetcode.algorithms.MediumMode;
+
+/**
+ * Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: left = 5, right = 7
+ * Output: 4
+ * Example 2:
+ *
+ * Input: left = 0, right = 0
+ * Output: 0
+ * Example 3:
+ *
+ * Input: left = 1, right = 2147483647
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= left <= right <= 231 - 1
+ */
+public class BitwiseANDOfNumbersRange {
+
+    /**
+     * Runtime
+     * 4 ms
+     * Beats
+     * 40.31%
+     * Memory
+     * 43.1 MB
+     * Beats
+     * 25.94%
+     */
+    public int rangeBitwiseAnd(int left, int right) {
+        int res = 0;
+        if(left == right){
+            res = left;
+        } else {
+            String binLeft = Integer.toBinaryString(left);
+            String binRight = Integer.toBinaryString(right);
+            if(binLeft.length() != binRight.length()){
+                return 0;
+            }
+            int i = 0;
+            while(i < binLeft.length()){
+                if(binLeft.charAt(i) == '1' && binRight.charAt(i) == '1'){
+                    res+=Math.pow(2, binLeft.length()-1-i++);
+                } else if(binLeft.charAt(i) == '0' && binRight.charAt(i) == '0'){
+                    i++;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+}
